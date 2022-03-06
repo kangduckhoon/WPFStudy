e3904435b344292624a6432a94d0b4fef2974d7b
Duckhoon, Kang <kdhkang@naver.com>
2022-03-07 00:21:05 +0900

    연습 중인 코드

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFStudy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            AboutUs window = new AboutUs();
+            window.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SignIn window = new SignIn();
+            _ = window.ShowDialog();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit?", "XOKR", MessageBoxButton.OKCancel, MessageBoxImage.Question, MessageBoxResult.Cancel) == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ SignIn.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFStudy
+{
+    /// <summary>
+    /// Interaction logic for SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Window
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit?", "XOKR", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.Yes)
+            {
+                //do something before exit
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                //do something before close
+                this.Close();
+            }
+        }
+    }
+}
